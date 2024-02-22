45fc6e54d01f6e8415200e80f2eb303631e028cb
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-02-22 13:45:57 +0000

    esercizio4 continuo1

--- esercizio-4/src/app/pag3/second3/second3.component.ts
+++ esercizio-4/src/app/pag3/second3/second3.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-second3',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './second3.component.html',
   styleUrl: './second3.component.css'
 })
