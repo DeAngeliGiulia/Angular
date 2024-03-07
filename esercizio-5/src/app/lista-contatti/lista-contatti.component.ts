1580800f17afdd02c3d0209f36118473b78d0fec
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2024-03-07 11:05:30 +0000

    esercizio5 continuo2

--- esercizio-5/src/app/lista-contatti/lista-contatti.component.ts
+++ esercizio-5/src/app/lista-contatti/lista-contatti.component.ts
@@ -16,10 +16,16 @@ export class ListaContattiComponent {
   panelOpenState = false;
   
   persone = [
-    {nome: "Tony", cognome: "Stark", immagine: "./assets/tony.jpg", numero: "3315946479"},
-    {nome: "", cognome: "", numero: ""},
-    {nome: "", cognome: "", numero: ""},
-    {nome: "", cognome: "", numero: ""},
+    {nome: "Tony", cognome: "Stark", immagine: "./assets/tony.jpg", numero: "3315946479", eta: "50", isOnline: false, colore: "yellow"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "47", isOnline: false, colore: "orange"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "50", isOnline: true, colore: "red"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "50", isOnline: false, colore: "deeppink"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "50", isOnline: false, colore: "blueviolet"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "50", isOnline: false, colore: "blue"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "50", isOnline: false, colore: "dodgerblue"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "50", isOnline: false, colore: "deepskyblue"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "50", isOnline: false, colore: "aqua"},
+    {nome: "Jack", cognome: "Sparrow", immagine: "./assets/Jacksparrow.jpg", numero: "3473786291", eta: "50", isOnline: false, colore: "chartreuse"},
   ]
 }
 
